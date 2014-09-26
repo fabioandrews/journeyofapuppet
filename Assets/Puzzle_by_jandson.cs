685783ebea52ca03d28046031d172b4af08ad95c
jandsoncavalcanti <jandsoncavalcanti@live.com>
2014-09-26 19:34:07 -0300

    Criado as testes para cenas
    Está praticamente vazia porque eu estou vendo como ajeitar a hierarquia

---
+++ Assets/Puzzle_by_jandson.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System;
+namespace AssemblyCSharp
+	
+{
+	public class Puzzle_by_jandson : MonoBehaviour {
+
+		private Tile_by_jandson[] ordem;
+		Vector3 auxiliar;
+		void  start(){
+
+			ordem = new Tile_by_jandson[9];
+			auxiliar.Set ((float)-1.1,(float) 1.1,(float) -1);
+
+			for (int counter1 = 0; counter1 < 3; counter1++)
+			{
+				for (int counter2 = 0; counter2 < 3; counter2++)
+				{
+					ordem[counter2+(counter1*3)].setPosicao(auxiliar);
+					auxiliar.Set(auxiliar.x+(float)1.08,auxiliar.y,auxiliar.z);
+				}
+				auxiliar.Set((float)-1.1,auxiliar.y+(float)1.05,auxiliar.z);
+			}
+		}
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+	
+		}
+	}
+}
---
+++ Assets/Tile_by_jandson.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+namespace AssemblyCSharp
+	
+{
+	public class Tile_by_jandson : MonoBehaviour {
+		private String acao;
+		private Vector3 posicao;
+		private Boolean clicado;
+	
+	
+		public Tile_by_jandson(Vector3 posicao_inicial)
+		{
+			clicado = false;
+			posicao = posicao_inicial;
+			transform.position = posicao;
+		}
+
+		public void setarAcao(String novaAcao)
+		{
+			acao = novaAcao;
+		}
+		public String getAcao()
+		{
+			return acao;
+		}
+	
+		public Vector3 getPosicao()
+		{
+			return posicao;
+		}
+
+		public void setPosicao(Vector3 novaposicao)
+		{
+			posicao = novaposicao;
+			transform.position = posicao;
+		}
+		public void OnMouseDown()
+		{
+			clicado = true;
+		}
+	
+		public Boolean getClicado()
+		{
+			return clicado;
+		}
+
+		public void setClicado()
+		{
+			clicado = !clicado;
+		}
+
+		// Use this for initialization
+		void Start () {
+	
+		}
+	
+	// Update is called once per frame
+		void Update () {
+	
+		}
+	}
+}
